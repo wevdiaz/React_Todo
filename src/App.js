919a9283212c4346e3ba47c916eaf16f47960e0b
Weverson Diazz <wevers.info@gmail.com>
2022-04-25 21:20:57 -0300

    Criando um container utilizando componente e props.children

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Hooks from './components/Hooks';
 import List from './components/List';
 import RenderCond from './components/RenderCond';
 import Fragments from './components/Fragments';
+import Container from './components/Container';
 
 function App() {
   return (
@@ -25,6 +26,10 @@ function App() {
      <RenderCond x={8} y={22} />
 
     <Fragments />
+
+    <Container>
+      <h1>Este é o filho do elemento pai.</h1>
+    </Container>
 
     </div>
   );
