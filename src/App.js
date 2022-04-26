96d7ead8552d9bf4323b3c8aefb1fb5b448ee217
Weverson Diazz <wevers.info@gmail.com>
2022-04-25 22:05:45 -0300

    Preparando a aplicação

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 
+import { useState, useEffect } from "react";
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
 
+const API = "http://localhost:5000";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+
   return (
     <div className="App">
      
-
+      <h1>Todo</h1>
 
     </div>
   );
