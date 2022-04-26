b3fb70706af698bec9ec85cec103b1f9c72b1a04
Weverson Diazz <wevers.info@gmail.com>
2022-04-26 14:31:15 -0300

    configurando e estilizando o Header

--- src/App.js
+++ src/App.js
@@ -8,11 +8,23 @@ const API = "http://localhost:5000";
 function App() {
   const [title, setTitle] = useState("");
   const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   return (
     <div className="App">
      
-      <h1>Todo</h1>
+      <div className='todo-header'>
+        <h1>React Todo</h1>
+      </div>
+
+      <div className='form-todo'>
+        <p>Formulário</p>
+      </div>
+
+      <div className='list-todo'>
+        <p>Lista</p>
+      </div>
 
     </div>
   );
