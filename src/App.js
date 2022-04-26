b20b6aed2fb91b9cda1338f0c91d3c8501f1bb83
Weverson Diazz <wevers.info@gmail.com>
2022-04-25 21:24:57 -0300

    criando a pasta de conceitos básicos da aula de React

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import './App.css';
 
-import FirstComponent from './components/FirstComponent';
-import AnotherComponent from './components/AnotherComponent';
-import Images from './components/Images';
-import Hooks from './components/Hooks';
-import List from './components/List';
-import RenderCond from './components/RenderCond';
-import Fragments from './components/Fragments';
-import Container from './components/Container';
+
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World!</h1>
+     
 
-     <FirstComponent />
-     <AnotherComponent />
-
-     <Images />
-
-     <Hooks />
-
-     <List />
-
-     <RenderCond x={8} y={22} />
-
-    <Fragments />
-
-    <Container>
-      <h1>Este é o filho do elemento pai.</h1>
-    </Container>
 
     </div>
   );
